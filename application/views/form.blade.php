4bfe2054c53b0377cf9759e434352da90373470d
pradanadhyaksa <danadhyaksa83@gmail.com>
2023-07-06 12:09:21 +0700

    UAS

--- application/views/form.blade.php
+++ application/views/form.blade.php
@@ -41,6 +41,16 @@
                   @endforeach
                 </select>
             </div>
+            <label for="exampleInputEmail1" class="form-label">Jenis</label>
+            <div>
+            <input type="radio" id="berita" name="fav_language" value="Berita"> 
+            <label for="berita">Berita</label>
+             <input type="radio" id="tutorial" name="fav_language" value="Tutorial">
+             <label for="tutorial">Tutorial</label>
+              <input type="radio" id="blog" name="fav_language" value="Blog">
+            <label for="blog">Blog</label>
+            </div>
+            <br>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Artikel</label>
                 <textarea class="form-control" id="nama" name="artikel" rows="3"></textarea>
